2d578ec135e31f848e003867b2f3604d445f8ea5
Sudip <codersudip07@gmail.com>
2026-06-12 17:17:41 +0530

    work section animation completed

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -1,3 +1,19 @@
-gsap.to({
-    
-})
+window.addEventListener("load", () => {
+    gsap.registerPlugin(ScrollTrigger);
+    const container = document.querySelector("#container");
+    const scrollAmount = container.scrollWidth - window.innerWidth;
+    // console.log("scrollAmount =", scrollAmount);
+    gsap.to(container, {
+        x: -scrollAmount,
+        ease: "none",
+        scrollTrigger: {
+            trigger: "#work-section",
+            start: "top top",
+            end: "+=" + scrollAmount,
+            scrub: 1,
+            pin: true,
+            invalidateOnRefresh: true,
+            // markers: true
+        }
+    });
+});
