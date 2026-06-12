4b15af0f0dd0fa1f6bd1e795a63639ae959ee9fb
Prithwijit4 <prithwijitkhan123@gmail.com>
2026-06-12 11:44:30 +0530

    upload again

--- assets/js/script.js
+++ assets/js/script.js
@@ -281,12 +281,12 @@ function startReveal() {
 
 
 var swiper = new Swiper(".mySwiper", {
-      direction: "vertical",
-      slidesPerView: 1,
-      spaceBetween: 30,
-      mousewheel: true,
-      pagination: {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+    pagination: {
         el: ".swiper-pagination",
         clickable: true,
-      },
-    });
+    },
+});
